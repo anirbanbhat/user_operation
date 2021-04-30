e9f2f0e6ca6de41acac6512bdf550a61a86b7d0b
Anirban Bhattacherji <anirbanbhattacherji@gmail.com>
2021-04-30 11:14:47 +0100

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:alpine3.7
+FROM python:3.8-slim-buster
+LABEL MAINTAINER="Anirban Bhattacherji <anirbanbhattacherji@gmail.com>"
 COPY . /app
 WORKDIR /app
-RUN apk add --no-cache --virtual .build-deps gcc musl-dev \
-     && pip install cython \
-     && apk del .build-deps gcc musl-dev
 RUN pip install -r requirements.txt
+RUN python setup.py install
 ENV PORT 5000
+ENV SERVER_NAME "0.0.0.0:5000"
 EXPOSE 5000
-ENTRYPOINT [ "python" ]
+ENTRYPOINT ["python"]
 CMD [ "userapp/app.py" ]
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='user_operation',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=['Flask', 'Flask-PyMongo', 'pymongo', 'bcrypt', 'jsonschema', 'flask-restplus==0.9.2',
-                      'Werkzeug==0.16.1', 'flask-swagger-ui==3.20.9', 'flask_cors==3.0.7']
+    install_requires=['Flask==1.1.2', 'pymongo', 'bcrypt==3.1.4', 'jsonschema', 'Werkzeug==0.16.1',
+                      'flask-swagger-ui==3.36.0']
 )
